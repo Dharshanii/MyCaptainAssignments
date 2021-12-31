6f449aa5d773172d196527c464956721cb0fca02
Dharshanii <96439490+Dharshanii@users.noreply.github.com>
2021-12-31 13:05:58 +0530

    Add files via upload

---
+++ mycaptain_area.py
@@ -0,0 +1,5 @@
+import math
+radius=float(input('Enter the radius: '))
+area=math.pi*radius*radius
+print('Area of the circle with radius ',radius,' is: ',area)
+
---
+++ mycaptain_extension.py
@@ -0,0 +1,3 @@
+file_name=input('Enter the name of the file: ')
+splitted=file_name.split('.')
+print('The extension is: ',splitted[1])
